ee1f644071a48e3270a0f1bac929d3330fd3cfdd
KaungMyatHun-15691 <kaungmyathun7@gmail.com>
2024-12-25 12:19:13 +0630

    update: change http to https at services

--- src/utils/service.ts
+++ src/utils/service.ts
@@ -13,11 +13,11 @@ export const debounceFunc = (callback : (...args:any) => void  , delay : number)
 const key = import.meta.env.VITE_API_KEY;
 
 export const getApiCall = (api: string = "") => {
-    const url = `http://api.weatherapi.com/v1/current.json?key=${key}&q=${api}&aqi=yes`
+    const url = `https://api.weatherapi.com/v1/current.json?key=${key}&q=${api}&aqi=yes`
     return axios.get(url)
 }
 
 export const getWeatherForecast = (api : string = "") => {
-    const url = `http://api.weatherapi.com/v1/forecast.json?key=${key}&q=${api}&days=7&aqi=yes&alerts=no`
+    const url = `https://api.weatherapi.com/v1/forecast.json?key=${key}&q=${api}&days=7&aqi=yes&alerts=no`
     return axios.get(url)
 }
