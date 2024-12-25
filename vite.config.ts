3488838211afa0f6ccb33e9d3c4139a1416b4d09
KaungMyatHun-15691 <kaungmyathun7@gmail.com>
2024-12-25 12:07:31 +0630

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test:{
-    global: true,
-    environment: "jsdom",
-    setupFiles: "./src/test/setup.ts"
-  }
+  plugins: [react()]
 })
